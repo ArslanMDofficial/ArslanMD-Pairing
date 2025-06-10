7cfeab41a9b1125c07ac529e96b1c5b0a0444b47
Arslan-MD <157588592+ArslanMDofficial@users.noreply.github.com>
2025-06-10 23:44:27 +0500

    Update generateCode.js

--- generateCode.js
+++ generateCode.js
@@ -1,10 +1,10 @@
-// generateCode.js
-function generatePairingCode(length = 8) {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let code = '';
-    for (let i = 0; i < length; i++) {
-        code += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return code;
+function generateCode(length = 8) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < length; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return code;
 }
-module.exports = generatePairingCode;
+
+module.exports = { generateCode };
