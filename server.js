d490169a2a9c4638530505ea6c64400d5add9d18
Arslan-MD <157588592+ArslanMDofficial@users.noreply.github.com>
2025-06-10 22:28:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Static serve
+const PORT = process.env.PORT || 3000; // <-- yeh line zaroori hai
+
 app.use(express.static(path.join(__dirname, 'client')));
 
-// Home page show karega index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
-// Your existing code (pairing endpoint, etc.) should be here
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
