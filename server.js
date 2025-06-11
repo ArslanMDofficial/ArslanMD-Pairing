7d0dece080ff151cad4a666b68c2a54b66a4114d
Arslan-MD <157588592+ArslanMDofficial@users.noreply.github.com>
2025-06-11 13:10:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const { pairBotWithNumber } = require("./pairBot");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("client"));
